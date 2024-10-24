684453f499e0bed3bff81f0314809b702ff69448
Jozef Lumaj <jozef.lumaj@gmail.com>
2024-10-23 23:01:32 -0400

    a lot of unfinished business here

--- src/basic_modules/mod.rs
+++ src/basic_modules/mod.rs
@@ -213,7 +213,7 @@ pub fn neighbourhood_update<T, N>(bmu:DVector<T>, bmu_index:Vec<usize>, map:DMat
     //now that neighbourhood sets are defined
     //update all values based on the following generalized formula:
         // current_element = current_element + changing_standardized_gaussian(x=neighbourhood_level)*(x_input - current_element)
-        // changing_standardized_gaussian:= 
+        // changing_standardized_gaussian:= gaussian starts wide during training, ends thin near end of training
     for j in 0..set_of_neighbourhoods.len() {
         //distance_calc(DistanceType::MatrixNeighbourhood, bmu, bmu);
     }
