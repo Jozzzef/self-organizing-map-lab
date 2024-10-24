a95133fde08f3e1f709a611511dc2ee59bb1fb1b
Jozef Lumaj <jozef.lumaj@gmail.com>
2024-10-23 22:29:37 -0400

    a lot of unfinished business here

--- src/basic_modules/mod.rs
+++ src/basic_modules/mod.rs
@@ -36,11 +36,19 @@ pub fn simple_som(
     let input_matrix = input_matrix.transpose();
     print!("{input_matrix}");
 
+
+    //loop starts here
+    //calculate Best Matching Unit, i.e. matching vector = the vector with the smallest distance to the input vector
+    //distance_calc(DistanceType::Euclidean);
+
+    //update neighbourhood
+    //neighbourhood_update();
+
     return map_matrix
 }
 
 
-// Helper Functions
+// Helper Functions ----------------------------------------------------------------
 
 pub fn read_csv_to_matrix(path:String) -> Result<DMatrix<f64>, Box<dyn Error>>  {
     
@@ -80,6 +88,8 @@ pub fn read_csv_to_matrix(path:String) -> Result<DMatrix<f64>, Box<dyn Error>>  
 }
 
 
+
+
 pub fn print_matrix_of_vectors(matrix: &DMatrix<DVector<f64>>, float_precision: usize){
     let nrows = matrix.nrows();
     let ncols = matrix.ncols();
@@ -103,11 +113,167 @@ pub fn print_matrix_of_vectors(matrix: &DMatrix<DVector<f64>>, float_precision: 
     println!("end {ncols} x {nrows} matrix");
 }
 
-pub fn euclidean_distance(){
-    
+
+
+
+pub enum DistanceType {
+    Euclidean,
+    Minkowski,
+    Correlation,
+    TanimotoSimilarity,
+    Levenshtein,
+    Entropy,
+    Hamming,
+    MatrixNeighbourhood
 }
 
-pub fn neighbourhood_update(){
-    
+pub fn distance_calc<T>(distance_type:DistanceType, v:DVector<T>, w:DVector<T>){
+
+    match distance_type {
+        DistanceType::Euclidean => {
+            println!("Handling Euclidean distance");
+            // Add your logic for Euclidean distance here
+        },
+        DistanceType::Minkowski => {
+            println!("Handling Minkowski distance");
+            // Add your logic for Minkowski distance here
+        },
+        DistanceType::Correlation => {
+            println!("Handling Correlation distance");
+            // Add your logic for Correlation distance here
+        },
+        DistanceType::TanimotoSimilarity => {
+            println!("Handling Tanimoto Similarity");
+            // Add your logic for Tanimoto Similarity here
+        },
+        DistanceType::Levenshtein => {
+            println!("Handling Levenshtein distance");
+            // Add your logic for Levenshtein distance here
+        },
+        DistanceType::Entropy => {
+            println!("Handling Entropy-based distance");
+            // Add your logic for Entropy here
+        },
+        DistanceType::Hamming => {
+            println!("Handling Hamming distance");
+            // Add your logic for Hamming distance here
+        },
+        DistanceType::MatrixNeighbourhood => {
+            println!("Handling Matrix Neighbourhood distance");
+            // Add your logic for Matrix Neighbourhood
+        }
+    }
 }
 
+
+
+
+pub fn neighbourhood_update<T, N>(bmu:DVector<T>, bmu_index:Vec<usize>, map:DMatrix<DVector<T>>) -> DMatrix<DVector<T>> {
+    
+    //**should probably memoize the neighbourhood set creation for all possible bmus
+
+    //create neighbourhood sets
+    //the elements are the indices (i,j) of each element within a neighbourhood, 
+    //the number of the nieghbouhood is ordered from 0 to k, where 0 is the bmu vector, k is the further neighbourhood
+    let mut set_of_neighbourhoods: Vec<Vec<Vec<usize>>> = Vec::new();
+    let mut index_while_loop: usize = 0;
+
+    loop {
+        if index_while_loop == 0 {
+            //bmu is the neighbourhood 0, wrap in another vector for type rules
+            set_of_neighbourhoods.push(vec![bmu_index.clone()]);
+        } else {
+            //build possible sets, same index as the bmu_index elements indexes
+            let mut possible_neigh_indices: Vec<Vec<usize>>  = Vec::new();
+            for index_val in &bmu_index {
+                //index_while_loop denotes the level of neighbourhood, the number of "steps" away it is from the bmu
+                let start = index_val - index_while_loop;
+                let end = index_val + index_while_loop + 1;
+                let vec_from_range: Vec<usize> = (start..end).collect();
+                possible_neigh_indices.push(vec_from_range)
+            }
+            
+            let mut neighbourhood_indices = cartesian_product(possible_neigh_indices);
+
+            for i in 0..index_while_loop {
+                //remove all the interior elements which belong to other neighbourhoods, since the cartesian product outputs them all
+                set_difference_for_nested_vectors(&mut neighbourhood_indices, &set_of_neighbourhoods[i]);
+            }
+
+            if neighbourhood_indices.len() == 0 {
+                break;
+            }
+
+            set_of_neighbourhoods.push(neighbourhood_indices);
+        }
+
+        index_while_loop += 1;
+    }
+
+    //now that neighbourhood sets are defined
+    //update all values based on the following generalized formula:
+        //
+    for j in 0..set_of_neighbourhoods.len() {
+        //distance_calc(DistanceType::MatrixNeighbourhood, bmu, bmu);
+    }
+
+    return map
+}
+
+
+
+
+pub fn get_best_matching_unit<T>(x: DVector<T>, map:&DMatrix<DVector<T>>) -> (DVector<T>, Vec<usize>){
+    //handle types automatically
+
+    //return (bmu vector, it's ordered indices to locate it within the map)
+    return (x, vec![1,2,3])
+}
+
+
+
+
+pub fn generalized_median<T>(batch_vectors: Vec<DVector<T>>) -> DVector<T>{
+    //handle types automatically
+
+    return batch_vectors[0]
+}
+
+
+
+
+pub fn cartesian_product(vectors: Vec<Vec<usize>>) -> Vec<Vec<usize>> {
+    // Start with an initial product containing one empty vector
+    let mut result: Vec<Vec<usize>> = vec![vec![]];
+
+    for vec in vectors {
+        // For each vector, create the new combinations
+        result = result.into_iter()
+                       .flat_map(|prev| {
+                           vec.iter().map(move |&x| {
+                               let mut new_combination = prev.clone();
+                               new_combination.push(x);
+                               new_combination
+                           }).collect::<Vec<Vec<usize>>>()
+                       })
+                       .collect();
+    }
+
+    result
+}
+
+
+
+
+pub fn set_difference_for_nested_vectors<T>(v: &mut Vec<Vec<T>>, w: &Vec<Vec<T>>) -> Option<T> 
+where
+    T: PartialEq,
+{
+    // Retain only those sub-vectors in `v` that are NOT present in `w`
+    v.retain(|vec_v| {
+        !w.iter().any(|vec_w| vec_v == vec_w)  // Remove if `vec_v` exists in `w`
+    });
+
+    // Since the mutation is done in-place, return None to indicate no value is returned
+    None
+}
