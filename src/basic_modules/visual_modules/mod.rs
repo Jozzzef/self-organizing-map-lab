4ee2d79b92aacac384d88a5291c2309d8d238c7f
Jozef Lumaj <jozef.lumaj@gmail.com>
2024-11-20 19:37:36 -0500

    need to debug filled() on HSLColor

--- src/basic_modules/visual_modules/mod.rs
+++ src/basic_modules/visual_modules/mod.rs
@@ -1,6 +1,6 @@
-use plotters::prelude::*;
+use plotters::{data::float, prelude::*};
 use nalgebra::{DMatrix, DVector};
-
+use std::cmp::max as tuple_max;
 
 pub fn vector_magnitude_for_hue(som_map: &DMatrix<DVector<f64>>) -> DMatrix<isize> {
     let mut hue_matrix : DMatrix<isize> = DMatrix::zeros(som_map.nrows(), som_map.ncols());
@@ -19,27 +19,34 @@ pub fn vector_magnitude_for_hue(som_map: &DMatrix<DVector<f64>>) -> DMatrix<isiz
     }
 
     hue_matrix = hue_matrix.add_scalar(hue_matrix.min()); //remove any potential negatives
-    let hue_max = hue_matrix.max();
-    let max_digits = hue_max.abs().to_string().len();
-    let mut hue_partition_for_360 = vec![Vec::new(); max_digits];
-    let mut hue_partition_for_360_without_index = vec![Vec::new(); max_digits];
+    let max_val_hue_matrix = hue_matrix.max();
+    let min_val_hue_matrix = hue_matrix.min();
+    let max_dim = tuple_max(hue_matrix.nrows() , hue_matrix.ncols()) as isize;
+    let partitions_tile_size = (max_val_hue_matrix - min_val_hue_matrix) / (max_dim as isize) + 1;
+    let mut hue_partition_for_360 = vec![Vec::new(); max_dim as usize];
+    let mut hue_partition_for_360_without_index = vec![Vec::new(); max_dim as usize];
 
     for i in 0..hue_matrix.nrows() {
         for j in 0..hue_matrix.ncols() {
             let hue: isize = hue_matrix[(i, j)].clone();
-            let num_digits = hue.abs().to_string().len();
-            hue_partition_for_360[num_digits-1].push( ((i,j), hue) );
-            hue_partition_for_360_without_index[num_digits-1].push(hue);
+            for k in 0..max_dim {
+                let min_k = min_val_hue_matrix + k * partitions_tile_size;
+                let max_k = min_val_hue_matrix + (k + 1)  * partitions_tile_size;
+                if min_k <= hue && hue < max_k {
+                    hue_partition_for_360[k as usize].push( ((i,j), hue) );
+                    hue_partition_for_360_without_index[k as usize].push(hue);
+                }
+            }
         }
     }
 
-    for i in 0..max_digits {
-        let w = hue_partition_for_360_without_index[i].clone();
+    for i in 0..max_dim {
+        let w = hue_partition_for_360_without_index[i as usize].clone();
         let input_max = *w.iter().max().unwrap() as f64;
         let input_min = *w.iter().min().unwrap() as f64;
-        let output_max = ((i+1) * 360 / max_digits) as f64;
-        let output_min = if i == 0 {0.0} else {(i * 360 / max_digits) as f64};
-        for (index, hue) in hue_partition_for_360[i].clone() { 
+        let output_max = ((i+1) * 360 / max_dim) as f64;
+        let output_min = if i == 0 {0.0} else {(i * 360 / max_dim) as f64};
+        for (index, hue) in hue_partition_for_360[i as usize].clone() { 
             let hue = hue as f64;
             let ratio: f64 = (hue - input_min) / (input_max - input_min);
             let result: f64 = output_min + ratio * (output_max - output_min);
@@ -71,22 +78,27 @@ pub fn basic_visualization(som_map: &DMatrix<DVector<f64>>,
     image_path: &str) 
     -> Result<(), Box<dyn std::error::Error>> {
     
+    let max_dim = tuple_max(som_map.nrows(), som_map.ncols());
+
     let canvas = SVGBackend::new(image_path, (800,800)).into_drawing_area();
     canvas.fill(&WHITE)?;
 
     let mut chart = ChartBuilder::on(&canvas)
         .caption("Basic SOM Chart", ("sans-serif", 30))
         .margin(5) //in pixels
-        .build_cartesian_2d(0..100, 0..100)?;
+        .build_cartesian_2d(0..max_dim, 0..max_dim)?;
     
     chart
         .configure_mesh() //configure mesh/grid
-            .x_labels(10) //label each dimension? param is the number of labels in the dimension
-            .y_labels(10)
+            .x_labels(max_dim / 2) //label each dimension? param is the number of labels in the dimension
+            .y_labels(max_dim / 2)
             .draw()?;
 
     let hue_matrix = vector_magnitude_for_hue(&som_map);
     let vec_hue_matrix = dmatrix_to_vec(&hue_matrix);
+    // for debugging
+    
+
     chart.draw_series(
         vec_hue_matrix
             .iter()
@@ -98,11 +110,17 @@ pub fn basic_visualization(som_map: &DMatrix<DVector<f64>>,
                     .map(move |(v, x)| (x, y, v))}
             )
             .map(|(x, y, v)| {
-                Rectangle::new(
+                let v_float = *v as f64;
+                println!("{x}, {y}, {v_float}");
+                let hsl_colors = HSLColor(v_float, saturation, lightness).filled();
+                println!("{:#?}", hsl_colors);
+                let mut rect = Rectangle::new(
                     [(x,y), (x+1, y+1)],
-                    HSLColor(*v as f64, saturation, lightness).filled()
-            )
-        }   
+                    HSLColor(v_float, saturation, lightness).filled()
+                );
+                rect.set_margin(1, 1, 1, 1);
+                return rect
+            }   
         ))?;
 
     canvas.present()?;
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,21 @@ mod basic_modules;
 use basic_modules::simple_som;
 use basic_modules::print_matrix_of_vectors;
 use basic_modules::basic_visualization;
+use std::env;
+
 
 //dependencies
-use nalgebra as nalg;
 use nalgebra::{DMatrix, DVector};
 use clearscreen;
 
-const IMAGE_PATH: &str = "./image_outputs/image.svg";
 
 fn main() {
     clearscreen::clear().expect("failed to clear screen"); //clear terminal
     //println!("Hello, world!");
     let som_output: DMatrix<DVector<f64>> = simple_som(String::from("example_inputs/simple_input.csv"), (9,9), None, None, None);
     print_matrix_of_vectors(&som_output, 1);
-    match basic_visualization(&som_output, 0.7, 0.9, IMAGE_PATH) {
+    let image_path : &str = concat!(env!("CARGO_MANIFEST_DIR"), "/image_outputs/image.svg");
+    match basic_visualization(&som_output, 0.7, 0.9, image_path) {
         Ok(()) => println!("Image Outputted"),
         Err(e) => eprintln!("Error: {}", e)
     }   
